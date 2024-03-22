d1c1078d44bb5857829558d5a5bf02119929fe45
Tutor de GIT <tutordegit@parquesoft.com>
2024-03-21 19:22:52 -0500

    .env file released

--- SAEF_Manager/settings.py
+++ SAEF_Manager/settings.py
@@ -10,21 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
+import os, environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DOWNLOAD_DIR = Path(os.path.join(BASE_DIR, 'downloads'))
 
+env = environ.Env(
+    DEBUG=(bool, False),
+    MAINTENANCE_MODE=(int),
+)
+
+environ.Env.read_env()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6njz^b%r@%py+4wokv%ufzz^k@k=jwy3ce6g8pq@9k*$u%o$#6'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -85,14 +92,7 @@ WSGI_APPLICATION = 'SAEF_Manager.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        'NAME': 'saef_db', # database name
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    "default": env.db(),
 }
 
 
